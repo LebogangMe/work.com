8adb0d32b687c407fa0cc3e3dfe443a63c42e32d
LebogangMe <113901552+LebogangMe@users.noreply.github.com>
2022-09-19 14:18:12 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const header=document.querySelector('.header .nav-bar .nav-list .navbar');
+const mobile_menu=document.querySelector('.header .nav-bar .nav-list .nul');
+const header=document.querySelector('.header .container');
+
+navbar.addEventListener('click', () => {
+    navbar.classList.toggle('active');   
+}); 
